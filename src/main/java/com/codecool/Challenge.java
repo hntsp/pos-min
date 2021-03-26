5c82be1a6c5a70bc0529a4f16efd4ff222dd07a5
hntsp <hantosp@protonmail.com>
2021-03-26 11:17:31 +0100

    Update Challenge.java

--- src/main/java/com/codecool/Challenge.java
+++ src/main/java/com/codecool/Challenge.java
@@ -4,5 +4,13 @@ public class Challenge {
 
     public static int minPositive(int a, int b) {
 
+        if (a < 0 && b > 0) return b;
+        else if (b < 0 && a > 0) return a;
+        else if (b == 0 && a > 0) return a;
+        else if (a == 0 && b > 0) return b;
+        else if (Math.min(a, b) >= 0) return Math.min(a, b);
+
+        return -1;
+
     }
 }
